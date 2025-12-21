29c1d066d334c97a55681df35cbad0a4b72d5fb2
thanuja <thanujakularathna119@gmail.com>
2025-12-21 13:45:13 +0530

    create deployment configs

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import path from "path";
 import { ENV } from "./lib/env.js";
 
 const app = express();
+const __dirname = path.resolve();
 
 console.log(ENV.PORT);
 console.log(ENV.DB_URL);
@@ -12,6 +14,13 @@ app.get("/health", (req, res) => {
   });
 });
 
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 app.listen(ENV.PORT, () => {
   console.log(`Server is running on port ${ENV.PORT}`);
 });
