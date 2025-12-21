8dd427e2c4a506c78e7ab0d9c33819193742475d
thanuja <thanujakularathna119@gmail.com>
2025-12-21 12:38:30 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { ENV } from "./lib/env.js";
+
+const app = express();
+
+console.log(ENV.PORT);
+console.log(ENV.DB_URL);
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    message: "Server is running",
+  });
+});
+
+app.listen(ENV.PORT, () => {
+  console.log(`Server is running on port ${ENV.PORT}`);
+});
