cedc6e4761ea291dcc249351532f2ae024929bc5
thanuja <thanujakularathna119@gmail.com>
2025-12-21 14:19:56 +0530

    database configured

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,10 @@
 import express from "express";
 import path from "path";
 import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 const __dirname = path.resolve();
-
-console.log(ENV.PORT);
-console.log(ENV.DB_URL);
 
 app.get("/health", (req, res) => {
   res.status(200).json({
@@ -21,6 +19,16 @@ if (ENV.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
-app.listen(ENV.PORT, () => {
-  console.log(`Server is running on port ${ENV.PORT}`);
-});
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () => {
+      console.log(`Server is running on port ${ENV.PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server", error);
+  }
+};
+
+startServer();
