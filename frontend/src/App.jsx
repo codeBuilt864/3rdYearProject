8e9f390fd4ee012e90f50845b46cd26567041d52
Nazmy <nazmyn47@gmail.com>
2026-01-10 19:15:46 +0530

    problem detail page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import HomePage from "./pages/HomePage";
 import DashboardPage from "./pages/DashboardPage";
 import AboutPage from "./pages/AboutPage";
 import ProblemsPage from "./pages/ProblemsPage";
+import ProblemPage from "./pages/ProblemPage";
 
 
 
@@ -23,8 +24,22 @@ function App() {
           path="/dashboard"
           element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} />
 
-        <Route path="/problems" element ={<ProblemsPage/>} />
-        <Route path="/about" element ={<AboutPage/>} />
+        <Route
+          path="/AboutPage"
+          element={isSignedIn ? <AboutPage /> : <Navigate to={"/"} />} />
+
+        <Route
+          path="/Problems"
+          element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
+
+        {/* <Route path="/problems" element ={<ProblemsPage/> } />
+
+        <Route path="/about" element ={<AboutPage/>} /> */}
+   
+
+        <Route path="/problem/:id" element={isSignedIn? <ProblemPage/> : <Navigate to={"/"}/>}/>
+
+       
 
         {/* <Route path="/homepage" element ={<HomePage/>} />
         <Route path="/dashbord" element ={<DashboardPage/>} /> */}
